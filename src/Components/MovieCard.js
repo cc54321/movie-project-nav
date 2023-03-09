e1ddcf8c9b0682f0440d4408db0895e31a049258
cc54321 <charles.cole@codeimmersives.com>
2023-03-09 17:09:09 -0500

    commit -a

---
+++ src/Components/MovieCard.js
@@ -0,0 +1,14 @@
+const MovieCard = (props) => {
+
+	const {movie} = props;
+
+	return (
+		<div className="movie-list-card">
+			<h2>{movie.Title}</h2>
+			<p>{movie.Director}</p>
+		</div>
+	)
+}
+
+export default MovieCard
+
